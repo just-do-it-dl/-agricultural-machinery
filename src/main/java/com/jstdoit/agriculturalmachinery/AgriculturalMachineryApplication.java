9def7acfe57f9b084a9033116f7664e71c4425bd
caiyf <778197259@qq.com>
2020-01-05 16:00:09 +0800

    init project

---
+++ src/main/java/com/jstdoit/agriculturalmachinery/AgriculturalMachineryApplication.java
@@ -0,0 +1,13 @@
+package com.jstdoit.agriculturalmachinery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriculturalMachineryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriculturalMachineryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jstdoit/agriculturalmachinery/AgriculturalMachineryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jstdoit.agriculturalmachinery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriculturalMachineryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
